5aa0e3d54219bee8078f15966ff1e5a62041bcc4
Gustavo Camalionti <gustavocamalionti@gmail.com>
2022-07-21 17:57:48 -0300

    ao pressionar enter - executar uma função  (não finalizado)
    Erro

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,18 +2,23 @@ const buttonAdicionar = document.getElementById("container__button-adicionar");
 const itensAdicionados = document.getElementById("container__form-itens--adicionar");
 const textoTarefa = document.getElementsByClassName("container__input-adicionar");
     
+buttonAdicionar.addEventListener('click', addItemButton);
 
-buttonAdicionar.addEventListener('click', addItem);
+textoTarefa.addEventListener('keypress', function(e) {
+    if(e.key === 'Enter') {
+        addItemButton();
+    }
+  });
+
 
 var contadorItens = 0
 var strTarefa
-function addItem() {
+function addItemButton() {
     strTarefa = textoTarefa[0].value.trim()
     if (strTarefa != "") {
         textoTarefa[0].value ="";
         itensAdicionados.innerHTML = "<li id = '" + contadorItens + "LI' + class='container__itens hide'><input type='checkbox' id='" + contadorItens + "' class='container__input-itens'><label for='" + contadorItens + "' class='container__label-itens'>" + strTarefa + "</label></li>" + itensAdicionados.innerHTML;
         var item = document.getElementById(contadorItens + "LI");
-        
         item.classList.remove("hide");
         item.classList.add("show");
         buttonAdicionar.disabled = true;
