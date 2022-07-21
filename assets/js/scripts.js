ee57a194f579e808030d5b934ebd58e099e451a3
Gustavo Camalionti <gustavocamalionti@gmail.com>
2022-07-21 17:15:58 -0300

    Alguns Pequenos Ajustes no JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,17 +8,21 @@ buttonAdicionar.addEventListener('click', addItem);
 var contadorItens = 0
 var strTarefa
 function addItem() {
-    strTarefa = textoTarefa[0].value
-    console.log(strTarefa)
-    itensAdicionados.innerHTML = "<li id = '" + strTarefa + "LI' + class='container__itens hide'><input type='checkbox' id='" + strTarefa + "' class='container__input-itens'><label for='" + strTarefa + "' class='container__label-itens'>" + strTarefa + "</label></li>" + itensAdicionados.innerHTML;
-    var item = document.getElementById(strTarefa + "LI");
-    item.classList.remove("hide");
-    item.classList.add("show");
-    buttonAdicionar.disabled = true;
-    setTimeout(function() {
+    strTarefa = textoTarefa[0].value.trim()
+    if (strTarefa != "") {
+        textoTarefa[0].value ="";
+        itensAdicionados.innerHTML = "<li id = '" + contadorItens + "LI' + class='container__itens hide'><input type='checkbox' id='" + contadorItens + "' class='container__input-itens'><label for='" + contadorItens + "' class='container__label-itens'>" + strTarefa + "</label></li>" + itensAdicionados.innerHTML;
+        var item = document.getElementById(contadorItens + "LI");
         
-        item.classList.remove("show");
-        buttonAdicionar.disabled = false;
-    }, 1100)
-    console.log(item)
+        item.classList.remove("hide");
+        item.classList.add("show");
+        buttonAdicionar.disabled = true;
+        setTimeout(function() {
+            item.classList.remove("show");
+            buttonAdicionar.disabled = false;
+        }, 1100)
+
+        contadorItens += 1
+        } 
+    
 };
