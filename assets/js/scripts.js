3f638bbd9d2de8dcab9551125f3b8ff0ce0986e3
Gustavo Camalionti <54083715+gustavocamalionti@users.noreply.github.com>
2022-07-22 00:17:29 -0300

    Ultimas estilizações e ajustes no JS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,25 @@
 const buttonAdicionar = document.getElementById("container__button-adicionar");
 const itensAdicionados = document.getElementById("container__form-itens--adicionar");
 const textoTarefa = document.getElementsByClassName("container__input-adicionar");
-    
+
+var contadorItens = 0
+var strTarefa
+
+
 buttonAdicionar.addEventListener('click', addItemButton);
 
-textoTarefa.addEventListener('keypress', function(e) {
+textoTarefa[0].addEventListener('keypress', function(e) {
+    
     if(e.key === 'Enter') {
         addItemButton();
+        e.preventDefault();
     }
   });
 
 
-var contadorItens = 0
-var strTarefa
+
 function addItemButton() {
+   
     strTarefa = textoTarefa[0].value.trim()
     if (strTarefa != "") {
         textoTarefa[0].value ="";
@@ -21,13 +27,21 @@ function addItemButton() {
         var item = document.getElementById(contadorItens + "LI");
         item.classList.remove("hide");
         item.classList.add("show");
+        
         buttonAdicionar.disabled = true;
+        textoTarefa[0].disabled = true;
         setTimeout(function() {
             item.classList.remove("show");
+            textoTarefa[0].disabled = false;
             buttonAdicionar.disabled = false;
-        }, 1100)
+            textoTarefa[0].focus()
 
+        }, 500)
         contadorItens += 1
         } 
     
 };
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
